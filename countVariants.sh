aa13e869251b681d95100dacb32902f926d8dcc9
Jenhan Tao <jenhantao@gmail.com>
2014-02-03 11:27:30 -0800

    initial commit of script files

---
+++ countVariants.sh
@@ -0,0 +1,23 @@
+# 1. uses grep to grab "KEEP" variants from vcv files (given in tsv format)
+# 2. calls python script for counting variants and computing statistics
+# 3. calls R scripts for visualizing variants
+
+# input three vcf files in the following order: NB_NT, NB_TP, NT_TP
+
+# calls to grep
+grep -v REJECT $1 > nb_nt_nonRejected.tsv
+grep -v REJECT $1 > nb_tp_nonRejected.tsv
+grep -v REJECT $1 > nt_tp_nonRejected.tsv
+
+# calls to python script
+python variantCounter.py nb_nt_nonRejected.tsv nb_tp_nonRejected.tsv nt_tp_nonRejected.tsv
+
+# remove extra tsv files
+rm nb_nt_nonRejected.tsv
+rm nb_tp_nonRejected.tsv
+rm nt_tp_nonRejected.tsv
+# calls to Rscript
+# produce plots for individual matched sets
+
+
+#produce plots for all matched sets
--- variantCounter.py
+++ variantCounter.py
@@ -1,9 +1,18 @@
 # computes statistics given three matched vcf files
 # samples given to mutect in this order: normal, tumor
-# input three vcf files in the following order: NB_NT, NB_TP, NT_T
 # key mutations by contig_position, normal_name
 # meta data to keep: dbsnp_site, covered, tumor_power, normal_power
 # sanity check all ref_alleles should be the same
 # actually compairing alt_alleles
 # t_lod_fstar (likelihood tumor event is real / likelihood event is sequencing error) is the core statistic
 # things to watch out for: contaminant fraction, contaminant_lod, context for gc content?
+
+# input three vcf files in the following order: NB_NT, NB_TP, NT_TP
+import sys
+# read in files
+nb_nt_file = open(sys.argv[1])
+
+nb_tp_file = open(sys.argv[2])
+
+nt_tp_file = open(sys.argv[3])
+
