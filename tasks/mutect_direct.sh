639f2995716828e479cb0f964238ef35933553c4
Jenhan Tao <jenhantao@gmail.com>
2014-03-12 13:33:57 -0400

    updated mutect to use intervals file

--- tasks/mutect_direct.sh
+++ tasks/mutect_direct.sh
@@ -21,6 +21,7 @@ java -Xmx${heap}m -Djava.io.tmpdir\=${tmp_folder}_mutect -jar ${mutect} \
 --analysis_type MuTect \
 --reference_sequence $REF \
 --dbsnp $DBSNP_mutect \
+--intervals $ExonFile \
 --input_file:normal ${normalFile} \
 --input_file:tumor ${tumorFile} \
 --out ${output_dir}/${sname}.call_stats.out \
